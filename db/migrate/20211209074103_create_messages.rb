b65b4a6d7c6b1a576dff96f7f442c84d336b70e7
Mao Torihata <4321namap@gmail.com>
2021-12-09 11:39:51 +0000

    [fix]マイグレーションファイルの追加途中

--- db/migrate/20211209074103_create_messages.rb
+++ db/migrate/20211209074103_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :room, null: false, foreign_key: true
+      t.text :body, null: false
 
       t.timestamps
     end
