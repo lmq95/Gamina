242f9b782f565c0196021fa2e8262e5b00713fb2
lmq95 <4321namap@gmail,com>
2021-12-15 15:59:38 +0000

    [add]フォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,14 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   
   #フォロー・フォロワー機能
+  #自分がフォローする（与フォロー側）
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #与フォロー関係を通じて参照する
   has_many :followings, through: :relationships, source: :followed
+  #自分はフォローされる〈被フォロー側）
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :reverse_of_relationships, source: :follwer
+  #被フォローを通じて参照
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   #DM機能
   has_many :entries, dependent: :destroy
@@ -33,5 +37,19 @@ class User < ApplicationRecord
   enum gender: { 女性: 0, 男性: 1}
   
   attachment :profile_image
+  
+  
+  #フォロー
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id)
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
   
 end
