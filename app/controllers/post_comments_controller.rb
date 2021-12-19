29df83a947bae754bbdf5136c807827671822624
lmq95 <4321namap@gmail,com>
2021-12-19 16:00:58 +0000

    [add]非同期作成途中

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -8,7 +8,6 @@ class PostCommentsController < ApplicationController
     @comment = @post_comment.post
     if @post_comment.save
       @comment.create_notification_comment!(current_user, @post_comment.id)
-      redirect_to post_path(@post.id)
     else
       render 'posts/show'
     end
@@ -18,7 +17,6 @@ class PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     post_comment = @post.post_comments.find(params[:id])
     post_comment.destroy
-    redirect_to request.referer
   end
   
   private
